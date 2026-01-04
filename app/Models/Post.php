5d720b4395263bf4406e489d2a1e65b3f624dad9
ArashFadaee <92361081+ARASHFADAEE@users.noreply.github.com>
2026-01-04 05:12:53 -0500

    relation post and user models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,16 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     protected $fillable = [
         'category_id',
         'user_id',
-        'user_id',
         'title',
         'slug',
         'body',
         'status',
     ];
+    public function user()
+    {
+        $this->BelongsTo(Post::class);
+
+    }
 }
