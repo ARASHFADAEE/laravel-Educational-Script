372ce2db46e587b5f1669d76f4d10f03dd08ffd3
ArashFadaee <92361081+ARASHFADAEE@users.noreply.github.com>
2026-01-07 12:31:13 -0500

    fix like buttom

--- resources/views/frontend/partials/Home/articles.blade.php
+++ resources/views/frontend/partials/Home/articles.blade.php
@@ -32,15 +32,7 @@
                                             <img src="{{asset('storage/')}}/{{$post->thumbnail}}"
                                                 class="w-full h-48 object-cover rounded-xl" alt="..." />
                                         </a>
-                                        <button type="button"
-                                            class="absolute left-3 -bottom-3 w-9 h-9 inline-flex items-center justify-center bg-secondary rounded-full shadow-xl text-muted transition-colors hover:text-red-500 z-10">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                                                fill="currentColor" class="w-5 h-5">
-                                                <path
-                                                    d="m9.653 16.915-.005-.003-.019-.01a20.759 20.759 0 0 1-1.162-.682 22.045 22.045 0 0 1-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 0 1 8-2.828A4.5 4.5 0 0 1 18 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 0 1-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 0 1-.69.001l-.002-.001Z">
-                                                </path>
-                                            </svg>
-                                        </button>
+
                                     </div>
                                     <div class="relative space-y-3 z-10">
                                         <h2 class="font-bold text-sm">
